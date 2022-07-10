7fbedc449a996c8cd346bdfd4d127b01e73224b2
Robby <tkdoyer@gmail.com>
2022-07-10 12:53:15 -0500

    fixed prompt issue

--- ProfilePrompts/Intern.js
+++ ProfilePrompts/Intern.js
@@ -1,21 +1,23 @@
 const internPrompts = [
-    {
-        type: 'input',
-        message: 'Intern Name?',
-        name: 'name',
-    }, {
-        type: 'input',
-        message: 'ID?',
-        name: 'ID',
-    }, {
-        type: 'input',
-        message: 'Email?',
-        name: 'Email',
-    }, {
-        type: 'input',
-        message: 'Schoo;?',
-        name: 'School',
-    }
+
+
+    // {
+    //     type: 'input',
+    //     message: 'Intern Name?',
+    //     name: 'name',
+    // }, {
+    //     type: 'input',
+    //     message: 'ID?',
+    //     name: 'ID',
+    // }, {
+    //     type: 'input',
+    //     message: 'Email?',
+    //     name: 'Email',
+    // }, {
+    //     type: 'input',
+    //     message: 'Schoo;?',
+    //     name: 'School',
+    // }
 ]
 
 exports.Intern = internPrompts
